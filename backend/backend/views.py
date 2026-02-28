c1955f196c7e9a7ccb91bbde9ae3948ac7f2c1de
Roy Hamlin <royhamlinjr7@gmail.comm>
2026-02-28 18:44:58 +0530

    add user registration API endpoint

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,6 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import RegisterSerializer
 
 @api_view(['GET'])
 def hello_api(request):
     return Response({"message":"Hello from Django API"})
+
+@api_view(['POST'])
+def register_user(request):
+    serializer=RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save() #saves user
+        return Response({"message":"User Registered Successfully!"}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
