47f61fa8707ea67fe878a07563a9c121d8163ed0
Roy Hamlin <royhamlinjr7@gmail.comm>
2026-02-28 19:09:11 +0530

    add required email field

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
     class Meta:
         model = User
         fields = ['username','email','password']
