3260c7a60a64b55d2a573e0ca76b8c6ebdf4630b
Roy Hamlin <royhamlinjr7@gmail.comm>
2026-02-28 19:17:12 +0530

    add error handling in login API

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth.models import User
 
 from .serializers import RegisterSerializer
 
@@ -20,4 +21,9 @@ def register_user(request):
 def basic_login(request):
     username = request.data.get('username')
     password = request.data.get('password')
-    
+
+    try:
+        user = User.objects.get(username=username, password=password)
+        return Response({"message":"Login successful"}, status=status.HTTP_200_OK)
+    except User.DoesNotExist:
+        return Response({"message":"Invalid Credentials"}, status=status.HTTP_400_BAD_REQUEST)
