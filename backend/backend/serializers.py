a568d8b7eb816ab0d7409e40e54e30825fdff7c2
Roy Hamlin <royhamlinjr7@gmail.comm>
2026-02-28 18:59:13 +0530

    fix: correct Meta class name in RegisterSerializer

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = User
         fields = ['username','email','password']
