d0c573db7556a799a3fbabc2cf02814501df796a
Roy Hamlin <royhamlinjr7@gmail.comm>
2026-03-01 13:37:52 +0530

    add RegisterPage component and render in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import RegisterPage from './RegisterPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>TEST</h1>
+      <RegisterPage/>
     </>
   )
 }
