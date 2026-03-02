5bd6ab0965bc2f696b03d7852567f9ba27e4b2bd
Roy Hamlin <royhamlinjr7@gmail.comm>
2026-03-03 00:36:49 +0530

    create JobsSerializer with created_by field support

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+from .models import Job
+
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
     class Meta:
         model = User
         fields = ['username','email','password']
+
+class JobsSerializer(serializers.ModelSerializer):
+    created_by = serializers.StringRelatedField()
+
+    class Meta:
+        model = Job
+        fields = '__all__'
