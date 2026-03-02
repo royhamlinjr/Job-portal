18dc3100377b7eff7f1ae0af0ef3f4de3c55b1ce
Roy Hamlin <royhamlinjr7@gmail.comm>
2026-03-02 23:34:04 +0530

    add LoginPage component and render in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import RegisterPage from './RegisterPage'
+import LoginPage from './LoginPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <LoginPage/>
       <RegisterPage/>
     </>
   )
